33d8fac447e75aea93fd6381bc96f54deed464b4
Oskari Pirhonen <xxc3ncoredxx@gmail.com>
2022-12-04 15:31:49 -0600

    Implement fork(2) with clone(2)

--- 02-clone/clone.asm
+++ 02-clone/clone.asm
@@ -1,16 +1,43 @@
-; assemble: nasm -f elf64 clone.asm
+; TODO:
+;   - print whether in parent or child after fork
 %include "lib.h"
 
 global _start
 
 section .text
 _start:
+    call fork
+
     mov rax, SYS_GETPID
     syscall
 
     mov rdi, rax
     call printu
 
+    mov rax, SYS_WRITE
+    mov rdi, STDOUT
+    mov rsi, .nl
+    mov rdx, 1
+    syscall
+
     mov rax, SYS_EXIT
     mov rdi, 0
     syscall
+
+.nl:                        ; Just store a newline
+    db  0x0a
+
+; Implement fork(2) with clone(2)
+; Basically just call clone(2) with flags set to SIGCHLD
+fork:
+    mov rax, SYS_CLONE
+    mov rdi, SIGCHLD        ; flags
+    mov rsi, NULL           ; new stack pointer (NULL means dup and use cow)
+    mov rdx, NULL           ; child's TID pointer (in parent) (unused)
+    mov r10, NULL           ; child's TID pointer (in child) (unused)
+    mov r8, NULL            ; pointer to new TLS (unused)
+    syscall
+
+    ret
+
+; vim: filetype=asm syntax=nasm:
--- include/lib.h
+++ include/lib.h
@@ -6,11 +6,16 @@
 %define SYS_OPEN    0x02
 %define SYS_CLOSE   0x03
 %define SYS_GETPID  0x27
+%define SYS_CLONE   0x38
 %define SYS_EXIT    0x3c
 
 %define STDOUT      1
 
 %define O_RDONLY    0o00    ; From bits/fcntl-linux.h
+
+%define SIGCHLD     17      ; From bits/signum-arch.h
+
+%define NULL        0
 
 extern printu
 
